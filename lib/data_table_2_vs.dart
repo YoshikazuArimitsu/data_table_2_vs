a51b4c8a22767696c778cea83c1e82f75c0457b2
Y.Arimitsu <yoshikazu_arimitsu@albert2005.co.jp>
2024-09-07 19:25:03 +0900

    仮想スクロール

---
+++ lib/data_table_2_vs.dart
@@ -0,0 +1,210 @@
+import 'dart:math' as math;
+import 'package:data_table_2/data_table_2.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DataTable2Vs extends StatefulWidget {
+  const DataTable2Vs(
+      {
+        super.key,
+
+        // DataTable
+        this.sortColumnIndex,
+        this.sortAscending = true,
+        this.onSelectAll,
+        this.decoration,
+        this.dataRowColor,
+        this.dataTextStyle,
+        this.headingRowColor,
+        this.headingRowHeight,
+        this.headingTextStyle,
+        this.horizontalMargin,
+        this.columnSpacing,
+        this.showCheckboxColumn = true,
+        this.showBottomBorder = false,
+        this.dividerThickness,
+        this.checkboxHorizontalMargin,
+        this.border,
+        required this.columns,
+        required this.rows,
+
+        // DataTable2
+        this.dataRowHeight,
+        this.fixedColumnsColor,
+        this.fixedCornerColor,
+        this.headingCheckboxTheme,
+        this.datarowCheckboxTheme,
+        this.checkboxAlignment = Alignment.center,
+        this.bottomMargin,
+        this.showHeadingCheckBox = true,
+        this.minWidth,
+        this.scrollController,
+        this.horizontalScrollController,
+        this.isVerticalScrollBarVisible,
+        this.isHorizontalScrollBarVisible,
+        this.empty,
+        this.smRatio = 0.67,
+        this.fixedTopRows = 1,
+        this.fixedLeftColumns = 0,
+        this.lmRatio = 1.2,
+        this.sortArrowAnimationDuration = const Duration(milliseconds: 150),
+        this.sortArrowIcon = Icons.arrow_upward,
+        this.sortArrowBuilder,
+        this.headingRowDecoration,
+
+        // DataTable2Vs
+        this.pageSize = 100,
+      }
+  );
+
+  // DataTable
+  final int? sortColumnIndex;
+  final bool sortAscending;
+  final ValueSetter<bool?>? onSelectAll;
+  final Decoration? decoration;
+  final MaterialStateProperty<Color?>? dataRowColor;
+  final TextStyle? dataTextStyle;
+  final MaterialStateProperty<Color?>? headingRowColor;
+  final double? headingRowHeight;
+  final TextStyle? headingTextStyle;
+  final double? horizontalMargin;
+  final double? columnSpacing;
+  final bool showCheckboxColumn;
+  final double? dividerThickness;
+  final bool showBottomBorder;
+  final double? checkboxHorizontalMargin;
+  final TableBorder? border;
+  final List<DataColumn2> columns;
+  final List<DataRow2> rows;
+
+  // DataTable2
+  final double? dataRowHeight;
+  final ScrollController? scrollController;
+  final ScrollController? horizontalScrollController;
+  final bool? isVerticalScrollBarVisible;
+  final bool? isHorizontalScrollBarVisible;
+  final Widget? empty;
+  final double smRatio;
+  final double lmRatio;
+  final int fixedTopRows;
+  final int fixedLeftColumns;
+  final Color? fixedColumnsColor;
+  final Color? fixedCornerColor;
+  final CheckboxThemeData? headingCheckboxTheme;
+  final double? minWidth;
+  final Alignment checkboxAlignment;
+  final bool showHeadingCheckBox;
+  final CheckboxThemeData? datarowCheckboxTheme;
+  final double? bottomMargin;
+  final Duration sortArrowAnimationDuration;
+  final IconData sortArrowIcon;
+  final Widget? Function(bool ascending, bool sorted)? sortArrowBuilder;
+  final BoxDecoration? headingRowDecoration;
+
+  // DataTable2Vs
+  final int pageSize;
+
+  @override
+  State<DataTable2Vs> createState() => _DataTable2VsState();
+}
+
+class _DataTable2VsState extends State<DataTable2Vs> {
+  int startRow = 0;
+  late ScrollController scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController = widget.scrollController ?? ScrollController();
+    scrollController.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    if(widget.scrollController == null) {
+      scrollController.dispose();
+    }
+    super.dispose();
+  }
+
+  void onScroll() {
+    final _startRow = startRow;
+    final offset = scrollController.offset;
+    final scrollExtent = (scrollController.position.maxScrollExtent - scrollController.position.minScrollExtent);
+    final extentByRow = scrollExtent / widget.pageSize;
+
+    // ↑
+    if(startRow > 0) {
+      if(offset <= scrollController.position.minScrollExtent) {
+        final newStartRow = math.max(startRow - widget.pageSize ~/ 2, 0);
+        setState(() {
+          startRow = newStartRow;
+        });
+
+        scrollController.jumpTo(scrollController.offset + (_startRow - newStartRow) * extentByRow);
+      }
+    }
+
+    // ↓
+    if(startRow < widget.rows.length - widget.pageSize) {
+      if(offset >= scrollController.position.maxScrollExtent) {
+        final newStartRow = math.min(startRow + widget.pageSize ~/ 2, widget.rows.length - widget.pageSize);
+        setState(() {
+          startRow = newStartRow;
+        });
+
+        scrollController.jumpTo(scrollController.offset + (_startRow - newStartRow) * extentByRow);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int endRow = math.min(startRow + widget.pageSize, widget.rows.length);
+
+    return DataTable2(
+      sortColumnIndex : widget.sortColumnIndex,
+      sortAscending : widget.sortAscending,
+      onSelectAll : widget.onSelectAll,
+      decoration : widget.decoration,
+      dataRowColor : widget.dataRowColor,
+      dataTextStyle : widget.dataTextStyle,
+      headingRowColor : widget.headingRowColor,
+      headingRowHeight : widget.headingRowHeight,
+      headingTextStyle : widget.headingTextStyle,
+      horizontalMargin : widget.horizontalMargin,
+      columnSpacing : widget.columnSpacing,
+      showCheckboxColumn : widget.showCheckboxColumn,
+      dividerThickness : widget.dividerThickness,
+      showBottomBorder : widget.showBottomBorder,
+      checkboxHorizontalMargin : widget.checkboxHorizontalMargin,
+      border : widget.border,
+      columns : widget.columns,
+      dataRowHeight : widget.dataRowHeight,
+      horizontalScrollController : widget.horizontalScrollController,
+      isVerticalScrollBarVisible : widget.isVerticalScrollBarVisible,
+      isHorizontalScrollBarVisible : widget.isHorizontalScrollBarVisible,
+      empty : widget.empty,
+      smRatio : widget.smRatio,
+      lmRatio : widget.lmRatio,
+      fixedTopRows : widget.fixedTopRows,
+      fixedLeftColumns : widget.fixedLeftColumns,
+      fixedColumnsColor : widget.fixedColumnsColor,
+      fixedCornerColor : widget.fixedCornerColor,
+      headingCheckboxTheme : widget.headingCheckboxTheme,
+      minWidth : widget.minWidth,
+      checkboxAlignment : widget.checkboxAlignment,
+      showHeadingCheckBox : widget.showHeadingCheckBox,
+      datarowCheckboxTheme : widget.datarowCheckboxTheme,
+      bottomMargin : widget.bottomMargin,
+      sortArrowAnimationDuration : widget.sortArrowAnimationDuration,
+      sortArrowIcon : widget.sortArrowIcon,
+      sortArrowBuilder : widget.sortArrowBuilder,
+      headingRowDecoration : widget.headingRowDecoration,
+
+      rows : widget.rows.sublist(startRow, endRow),
+      scrollController : scrollController,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
+import 'data_table_2_vs.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,21 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -57,24 +34,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _incrementCounter1000() {
-    setState(() {
-      _counter+=1000;
-    });
-  }
-
-  void _incrementCounter10000() {
-    setState(() {
-      _counter+=10000;
-    });
-  }
 
   DataRow2 _row(int index) {
     return DataRow2(
@@ -88,26 +47,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
         body: Padding(
           padding: const EdgeInsets.all(16),
           child: Theme(
-              // Using themes to override scroll bar appearence, note that iOS scrollbars do not support color overrides
               data: ThemeData(
                   iconTheme: const IconThemeData(color: Colors.white),
                   scrollbarTheme: ScrollbarThemeData(
@@ -115,8 +62,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     // thumbVisibility: MaterialStateProperty.all(true),
                     // thumbColor: MaterialStateProperty.all<Color>(Colors.yellow)
                   )),
-              child: DataTable2(
-                // Forcing all scrollbars to be visible, alternatively themes can be used (see above)
+              child:
+                // DataTable2(
+                DataTable2Vs(
+
                 headingRowColor:
                     WidgetStateColor.resolveWith((states) => Colors.grey[850]!),
                 headingTextStyle: const TextStyle(color: Colors.white),
@@ -137,14 +86,12 @@ class _MyHomePageState extends State<MyHomePage> {
                     horizontalInside:
                         const BorderSide(color: Colors.grey, width: 1)),
                 dividerThickness: 1,
-                // this one will be ignored if [border] is set above
                 bottomMargin: 10,
                 minWidth: 900,
                 columns: const [
                   DataColumn2(
                     label: Text('Col1'),
                     size: ColumnSize.S,
-                    // example of fixed 1st row
                     fixedWidth: 200,
                   ),
                   DataColumn2(
@@ -163,6 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 rows: List<DataRow2>.generate(_counter, (index) => _row(index)),
               )),
         ),
+
         floatingActionButton: Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
